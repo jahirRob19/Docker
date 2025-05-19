0c96b33e1e55c6b86f7dcec12cd0d5736ed35c32
jahirRob19 <131838477+jahirRob19@users.noreply.github.com>
2025-05-19 14:08:43 -0600

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-<<<<<<< HEAD
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-  plugins: [sveltekit()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      reporter: ['text', 'json', 'html'],
-      include: ['src/**/*.{ts,js,svelte}']
-    }
-  }
-=======
 import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [sveltekit()]
->>>>>>> 56fc7ca50928c5695b9926dc53084eda604f2457
 });
